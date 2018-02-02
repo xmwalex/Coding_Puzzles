c0465d6cee7bfeb61fbf75070e158c2461d1504a
alex <xmwalex@gmail.com>
2018-02-01 17:34:39 -0800

    l4

--- Median_of_Tow_Sorted_Arrays.java
+++ Median_of_Tow_Sorted_Arrays.java
@@ -1,9 +1,6 @@
-#Median of Two Sorted Arrays
 #There are two sorted arrays nums1 and nums2 of size m and n respectively.
 
 #Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
-
-
 
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
@@ -43,4 +40,3 @@ class Solution {
         return 0;
     }
 }
-
